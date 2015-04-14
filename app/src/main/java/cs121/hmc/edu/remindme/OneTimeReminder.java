d9c9caa10866d2f9d022c3dfeb20c24991e41eb3
Tyra He <tyransfz@gmail.com>
2015-04-14 00:17:23 -0700

    Snooze and Swipe-to-Dismiss

--- app/src/main/java/cs121/hmc/edu/remindme/OneTimeReminder.java
+++ app/src/main/java/cs121/hmc/edu/remindme/OneTimeReminder.java
@@ -142,6 +142,6 @@ public class OneTimeReminder implements ReminderTime {
     @Override
     public boolean hasNextTime() {
         //check if the current time  is not past the reminder date
-        return !date.before(Calendar.getInstance());
+        return date.getTimeInMillis() + snoozeCounter * minToMillis > Calendar.getInstance().getTimeInMillis();
     }
 }
